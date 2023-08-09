d03e27f02f5a0f018f27e592d18ab0b2f824b870
Jaidev Shah <jaidevshah@Jaidevs-MacBook-Pro.local>
2023-08-09 11:53:23 -0400

    added comments

--- BigIntegerBillboards.java
+++ BigIntegerBillboards.java
@@ -21,28 +21,37 @@ public static long billboards(int k, List<Integer> revenue) {
     }
 
     BigInteger[] dp = new BigInteger[N];
-    //dp[i] represents the minimum loss when we remove revenue i and the positions before optimally to respect the k constraint. That is, the smallest amount of money that company has to lose by removing k billboards in a row from revenue[0, i]. 
+    //dp[i] represents the minimum loss when we remove revenue i and the positions before optimally to respect the k constraint. That is, the smallest amount of money that company has to lose by removing billboard at position i and removals before that are optimal.  
     
     if (k == N) {
         return total.longValue();
     } 
-    
+
+    //Base Case , initialize upto dp[k], or the first k + 1 elements of dp
     for (int i = 0; i < k + 1; i++) {
         dp[i] = BigInteger.valueOf(revenue_arr[i]);
     }
+
     
     BigInteger min_val = dp[0];
     int min_idx = 0;
     
     // dp[i] = min(dp[i - k - 1], ..., dp[i - 1]) + revenue[i]
+    // dp[i] means: remove optimally any position from [i - k - 1 , i) and remove at positon i  
 
-    //The double for loop computes the minimum min(dp[i - k - 1 : i])
-    //O(nk)
+
+
+    //O(nk) 
+
+
+    //Sliding window of size k+1 
+    //start to fill in from dp[k+1], 
     for (int i = k + 1; i < N; i++) {
         if (i - min_idx >= k) {
             min_val = dp[i - (k + 1)];
             min_idx = i - (k + 1);
 
+            //The inner for loop simply computes the minimum, in python its a one-liner min(dp[i - k - 1 : i]) (find minimum looking back k + 1 positions, excluding i)
             for (int j = i - k; j < i; j++) {
                 if (dp[j].compareTo(min_val) < 0) {
 
@@ -50,11 +59,13 @@ public static long billboards(int k, List<Integer> revenue) {
                     min_idx = j;
                 }
             }
-        }
+        } //Using a MinQueue can help keep track of the minimum element in a sliding window of size k + 1 in O(1) time
+
+
         //dp[i] = min(dp[i - k - 1], ..., dp[i - 1]) + revenue[i]
         dp[i] = min_val.add(BigInteger.valueOf(revenue_arr[i]));
 
-        System.out.println("dp[" + i + "] = " + dp[i]);
+        //System.out.println("dp[" + i + "] = " + dp[i]);
         
         //update min_val and min_index
         if (dp[i].compareTo(min_val) < 0) {
@@ -63,8 +74,16 @@ public static long billboards(int k, List<Integer> revenue) {
         }
     }
     
-    // minimum revenue loss between N - (k + 1) and N - 1 (end of array)
+    // minimum revenue loss between N - (k + 1) and N - 1 (end of array) (last k position )
+
+    //dp[N-1] is a valid solution, this contains the min_revenue_loss such that N-1 th billboard is removed and everything before is removed optimally, and k-constraint is satisifed
+    //What if N-1 is expensive to remove
+    //Any of the last k billboards can be removed and k-constraint is still satisfied.
+    //Might as well choose the dp[i] that costs the least aka has the minimum revenue loss
+
+    //choose the minimum dp[i]
     BigInteger min_rev_loss = dp[N - (k + 1)];
+
     for (int i = N - k; i < N; i++) {
         if (dp[i].compareTo(min_rev_loss) < 0) {
             min_rev_loss = dp[i];
@@ -127,6 +146,15 @@ public static void main(String[] args) {
 1
 6
 10
+
+Maximizing, equivalent to minimizing the loss of removed billboards 
+
+revnenues = [ _, 2, 3, _, 6, 10]
+k =2 
+min_loss = 2 
+profit = sum(revenue) - min_loss
+
+5 + 6 + 10 = 21 
 
 
 6 2
